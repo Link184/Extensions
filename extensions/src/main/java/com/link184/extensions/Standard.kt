ce369d981666a64d04031af473315d55ba7517ff
Eugeniu Tufar <eugeniu.tufar@ness.com>
2019-02-11 16:48:00 +0200

    Added extension to set private fields

--- extensions/src/main/java/com/link184/extensions/Standard.kt
+++ extensions/src/main/java/com/link184/extensions/Standard.kt
@@ -1,3 +1,5 @@
+@file:JvmName("Standard")
+
 package com.link184.extensions
 
 import android.util.Base64
@@ -28,10 +30,17 @@ fun Date.toCalendar(): Calendar = Calendar.getInstance().apply {
     time = this@toCalendar
 }
 
-inline fun <reified T: Any> T.getPrivateMember(memberName: String) =
-        T::class.declaredMemberProperties
-                .firstOrNull { it.name == memberName }
-                ?.let {
-                    it.isAccessible = true
-                    it.get(this@getPrivateMember)
-                }
+inline fun <reified T : Any> T.getPrivateMember(memberName: String) =
+    T::class.declaredMemberProperties
+        .firstOrNull { it.name == memberName }
+        ?.let {
+            it.isAccessible = true
+            it.get(this@getPrivateMember)
+        }
+
+fun Any.setPrivateMember(fieldName: String, newValue: Any?) =
+    this::class.java.getDeclaredField(fieldName)
+        .apply {
+            isAccessible = true
+            set(this@setPrivateMember, newValue)
+        }
